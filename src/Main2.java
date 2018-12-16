1c34740d81e9a37ee61746e41497cea9b06d7f11
Krzysztof Tomala <krzysiektom@op.pl>
2018-12-16 23:35:17 +0100

    Zadanie 3

---
+++ src/Main2.java
@@ -0,0 +1,7 @@
+public class Main2 {
+    public static final String reg1="^[-+]?(0{1}|[1-9]\\d*)?(\\.\\d*$)?";
+    public static final String reg2="^[-+]?([0-9])?(\\.\\d+)?(e(-)?\\d*)?";
+    public static final String reg3="(0[1-9]|[1-2][0-9]|3[0-1])\\/(0[1-9]|1[0-2])\\/\\d{4}";
+    public static final String reg4="[2-9]\\d{3}-(0[1-9]|1[0-2])-(0[1-9]|[1-2][0-9]|3[0-1])";
+
+}
